63078ba4b4f776a1340f7c4da61e894a1ca7d8dc
Henry Chen <henchen@seas.upenn.edu>
2023-02-10 17:11:51 -0500

    removed git ignore stuff

--- dist/game/loggable.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Interface declaring the class loggable. Every class that
- * implements this interface must implement a method `toLog` which
- * returns a string of the class' state.
- */
-export interface Loggable {
-    toLog: () => string;
-}
-export default Loggable;
