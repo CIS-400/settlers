6f29cbbe1e0c75cace8a72e269d7588090309c65
Aydan Gooneratne <aydangooneratne@Aydans-MacBook-Pro.local>
2023-03-20 17:07:01 -0400

    trade offer toLog

--- src/game/trade_offer.ts
+++ src/game/trade_offer.ts
@@ -1,6 +1,6 @@
-import { NUM_PLAYERS } from './constants'
-import Loggable from './loggable'
-import ResourceBundle from './resource/resource_bundle'
+import { NUM_PLAYERS } from "./constants";
+import Loggable from "./loggable";
+import ResourceBundle from "./resource/resource_bundle";
 
 export enum TradeStatus {
   Pending,
@@ -9,19 +9,19 @@ export enum TradeStatus {
 }
 export class TradeOffer implements Loggable {
   /** Unique number for the trade offer */
-  readonly id: number
+  readonly id: number;
   /** Player number who is making the offer. */
-  readonly offerer: number
+  readonly offerer: number;
   /** What is offered. */
-  readonly offer: ResourceBundle
+  readonly offer: ResourceBundle;
   /** What is requested in return. */
-  readonly request: ResourceBundle
+  readonly request: ResourceBundle;
   /** How each player number views the trade. */
   readonly status: {
-    [key: number]: TradeStatus
-  }
+    [key: number]: TradeStatus;
+  };
   /** Declines needed to automatically close the trade */
-  private maxDeclines: number
+  private maxDeclines: number;
 
   constructor(
     id: number,
@@ -30,39 +30,47 @@ export class TradeOffer implements Loggable {
     offer: ResourceBundle,
     request: ResourceBundle
   ) {
-    const isHost = host === offerer
-    this.id = id
-    this.offerer = offerer
-    this.offer = offer
-    this.request = request
-    this.maxDeclines = isHost ? NUM_PLAYERS - 1 : 1
+    const isHost = host === offerer;
+    this.id = id;
+    this.offerer = offerer;
+    this.offer = offer;
+    this.request = request;
+    this.maxDeclines = isHost ? NUM_PLAYERS - 1 : 1;
     if (!isHost) {
-      this.status = { [host]: TradeStatus.Pending }
+      this.status = { [host]: TradeStatus.Pending };
     } else {
-      this.status = {}
+      this.status = {};
       for (let i = 0; i < NUM_PLAYERS; i++) {
-        if (i === host) continue
-        this.status[i] = TradeStatus.Pending
+        if (i === host) continue;
+        this.status[i] = TradeStatus.Pending;
       }
     }
   }
 
   public allDeclined = () => {
-    let count = 0
+    let count = 0;
     for (let status in this.status) {
-      if (this.status[status] === TradeStatus.Decline) count++
+      if (this.status[status] === TradeStatus.Decline) count++;
     }
-    return count === this.maxDeclines
-  }
+    return count === this.maxDeclines;
+  };
 
-  toLog = () => ''
-  // `{\nid: ${this.id}, offerer: ${this.offerer}\nstatus: ${this.status
-  //   .map((elt) => {
-  //     if (elt === TradeStatus.Accept) return 'Accept'
-  //     else if (elt === TradeStatus.Decline) return 'Decline'
-  //     return 'Pending'
-  //   })
-  //   .join(', ')}\noffer: [ ${this.offer.toLog()} ] request: [ ${this.request.toLog()} ]\n}`
+  toLog = () =>
+    `{\nid: ${this.id}, offerer: ${this.offerer}\nstatus: ${Object.entries(
+      this.status
+    )
+      .map(([player, status]) => {
+        `${player}: ${
+          status === TradeStatus.Accept
+            ? "Accepted"
+            : status === TradeStatus.Decline
+            ? "Declined"
+            : "Pending"
+        }`;
+      })
+      .join(
+        ", "
+      )}\noffer: [ ${this.offer.toLog()} ] request: [ ${this.request.toLog()} ]\n}`;
 }
 
-export default TradeOffer
+export default TradeOffer;
